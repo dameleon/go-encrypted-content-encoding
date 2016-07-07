6eee22ea5fcabe2ea2182701ee324243a4a29d65
dameleon <dameleon@gmail.com>
2016-07-07 22:07:29 +0900

    wip

--- main.go
+++ main.go
@@ -45,27 +45,9 @@ func Encrypt(p EncryptParams) {
 }
 
 
-func EncryptDeriveKey(p EncryptParams) ([]byte, []byte) {
-	var secret []byte
-	var context = []byte("")
-	
-	if p.key != nil {
-		secret = p.key
-	} else if p.dh != nil {
-			
-	} else if p.keyid != nil {
-		var err error
-		secret, _, err = holder.Get(p.keyid)
-		if err != nil {
-			log.Fatal("undefined keyid")
-		}
-	}
-	
-	if p.authSecret != nil {
-		auth := hkdf.New(sha256.New, secret, p.authSecret, BuildInfo([]byte("auth"), []byte("")))
-		auth.Read(secret)
-	}
-	
+func deriveKeyAndNonce(p EncryptParams, mode string) ([]byte, []byte) {
+	var salt = extractSalt(p.salt)
+	var secret, context = extractSecretAndContext(p, mode)
 	var keyInfo, nonceInfo []byte
 	
 	switch p.padSize {
@@ -76,23 +58,50 @@ func EncryptDeriveKey(p EncryptParams) ([]byte, []byte) {
 		keyInfo = []byte("Content-Encoding: aesgcm128")
 		nonceInfo = []byte("Content-Encoding: nonce")
 	}
-	
-	hkdfKey := hkdf.New(sha256.New, secret, p.salt, keyInfo)
-	hkdfInfo := hkdf.New(sha256.New, secret, p.salt, nonceInfo)
+	hkdfKey := hkdf.New(sha256.New, secret, salt, keyInfo)
+	hkdfNonce := hkdf.New(sha256.New, secret, salt, nonceInfo)
 	key := make([]byte, KEY_LENGTH)
-	info := make([]byte, NONCE_LENGTH)
+	nonce := make([]byte, NONCE_LENGTH)
 	hkdfKey.Read(key)
-	hkdfInfo.Read(info)
-	return key, info
+	hkdfNonce.Read(nonce)
+	return key, nonce
 }
 
-func DeriveDH(p EncryptParams, mode string, keyid, dh []byte) ([]byte, []byte) {
-	var senderPubkey, receiverPubkey []byte
+func extractSecretAndContext(p EncryptParams, mode string) ([]byte, []byte) {
+	var secret []byte
+	var context []byte
 	
+	if p.key != nil {
+		secret = extractSalt(p.key)
+	} else if p.dh != nil {
+		secret, context = extractDH(p.keyid, p.dh, mode)
+	} else if p.keyid != nil {
+		var err error
+		secret, _, err = holder.Get(p.keyid)
+		if err != nil {
+			log.Fatal("undefined keyid")
+		}
+	}
+	if secret == nil {
+		log.Fatal("null secret")
+	}
+	if p.authSecret != nil {
+		var authSecret []byte
+		if _, err := base64.URLEncoding.Decode(authSecret, p.authSecret); err != nil {
+			log.Fatal("cannot decode authSecret")
+		}
+		auth := hkdf.New(sha256.New, secret, authSecret, BuildInfo([]byte("auth"), []byte{}))
+		auth.Read(secret)
+	}
+	return secret, context
+}
+
+func extractDH(keyid, dh []byte, mode string) ([]byte, []byte) {
 	key, label, err := holder.Get(keyid)
 	if err != nil {
 		log.Fatal("undefined keyid")
 	}
+	var senderPubkey, receiverPubkey []byte
 	switch mode {
 	case "encrypt":
 		senderPubkey = key
@@ -101,8 +110,11 @@ func DeriveDH(p EncryptParams, mode string, keyid, dh []byte) ([]byte, []byte) {
 		senderPubkey = dh
 		receiverPubkey = key
 	}
-	
-	return ecdh.Curve25519().ComputeSecret(key, dh), append(label, append(lengthPrefix(receiverPubkey), lengthPrefix(senderPubkey)...)...)
+	buf := bytes.Buffer{}
+	buf.Write(label)
+	buf.Write(lengthPrefix(receiverPubkey))
+	buf.Write(lengthPrefix(senderPubkey))
+	return ecdh.Curve25519().ComputeSecret(key, dh), buf.Bytes()
 }
 
 func lengthPrefix(b []byte) []byte {
@@ -116,20 +128,19 @@ func BuildInfo(base, context []byte) []byte {
 	buf := bytes.Buffer{}
 	buf.Write([]byte("Content-Encoding: "))
 	buf.Write(base)
-	buf.Write([]byte{0})
 	buf.Write(context)
 	return buf.Bytes()
 }
 
-func ExtractSalt(salt []byte) []byte {
-	s, err := base64.URLEncoding.DecodeString(string(salt))
-	if err != nil {
+func extractSalt(salt []byte) []byte {
+	var s []byte
+	if _, err := base64.URLEncoding.Decode(s, salt); err != nil {
 		log.Fatal(err)
 	}
 	if len(s) != KEY_LENGTH {
 		log.Fatal("mismatch salt length")
 	}
-	return []byte(s)
+	return s
 }
 
 func CreateSalt() ([]byte, error) {
@@ -139,3 +150,9 @@ func CreateSalt() ([]byte, error) {
 	}	
 	return s, nil
 }
+
+func encrypt(buf []byte, p EncryptParams) {
+	var key, nonce = deriveKeyAndNonce(p, "encrypt")
+	var rs = p.rs
+}
+
